1dccf11112a2919f4fda020f50b105d95ad8d3f5
Suleyman <43322192+AM1G0S@users.noreply.github.com>
2025-07-30 20:21:54 +0300

    Add SegmentedControl component

---
+++ src/components/SegmentedControl/SegmentedControl.stories.tsx
@@ -0,0 +1,46 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { useState } from 'react';
+
+import { hideArgsControl } from '../../../.storybook/shared/args-manager';
+import { SegmentedControl, type SegmentedControlProps } from './SegmentedControl';
+
+const meta = {
+  title: 'Navigation/SegmentedControl',
+  component: SegmentedControl,
+  argTypes: hideArgsControl(['children'])
+} satisfies Meta<typeof SegmentedControl>;
+
+export default meta;
+
+const labels = [
+  { label: 'Label', value: 'label' },
+  { label: 'Label 2', value: 'label2' },
+  { label: 'Label 3', value: 'label3' }
+];
+
+export const Playground: StoryObj<SegmentedControlProps> = {
+  render: (args) => {
+    const [selected, setSelected] = useState(labels[0].value);
+
+    return (
+      <SegmentedControl {...args}>
+        {labels.map(({ value, label }) => (
+          <SegmentedControl.Item
+            key={value}
+            selected={selected === value}
+            onClick={() => { setSelected(value); }}
+          >
+            {label}
+          </SegmentedControl.Item>
+        ))}
+      </SegmentedControl>
+    );
+  },
+  decorators: [
+    (Component) => (
+      <div style={{ width: 500 }}>
+        <Component />
+      </div>
+    )
+  ]
+};
